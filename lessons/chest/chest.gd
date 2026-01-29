a8166ade608e543a200dbbbfa436377bfdc47e41
llpplj1001 <juyu7@s.sfusd.edu>
2026-01-29 13:18:21 -0800

    M6 L5

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,7 +1,11 @@
 #get_node = $ pretty much a shortcut
 extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func open() -> void:
+	animation_player.play("open")
+	
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -23,3 +27,12 @@ func _on_mouse_exited() -> void:
 	
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	if event_is_mouse_click:
+		open()
